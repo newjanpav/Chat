bfd26a02c2120fabf8a4efcabc4bee8675b0562f
Pavel Shymanski <Pavel Shymanski@gamil.com>
2023-05-17 23:38:57 +0300

    add SignUpVC, LogInVC,ChatVC

---
+++ Chat/SignUpViewController/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  Chat
+//
+//  Created by Pavel Shymanski on 17.05.23.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
