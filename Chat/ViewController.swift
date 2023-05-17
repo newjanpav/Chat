c87ece13e906e9db7ac01e675a84b42b8a10d8f4
Pavel Shymanski <Pavel Shymanski@gamil.com>
2023-05-17 22:31:56 +0300

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Pavel Shymanski on 17.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
